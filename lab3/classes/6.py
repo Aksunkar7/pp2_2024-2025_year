41e4344039537300a994598b3bbb3a2cbf1b6cbd
Aksunkar7 <akoking890@gmail.com>
2025-02-02 21:54:44 +0500

    Сhange of 6th task

--- lab3/classes/6.py
+++ lab3/classes/6.py
@@ -1,8 +1,17 @@
-is_prime = lambda n: n > 1 and all(n % i != 0 for i in range(2, n))
+class Prime:
+    def __init__(self, numbers):
+        self.numbers = numbers
+    
+    def isPrime(self):
+        is_prime = lambda n: n > 1 and all(n % i != 0 for i in range(2, n)) 
 
+        # сортировка жасау filter - массивті толық is_prime қанағаттанатын мәндермен толтырады
+        prime_numbers = list(filter(is_prime, numbers)) #жаңа массивке салдық
+        return prime_numbers
+        
+        
+    
 numbers = list(map(int, input("Сандарды енгізіңіз:").split()))
 
-# сортировка жасау filter - массивті толық is_prime қанағаттанатын мәндермен толтырады
-prime_numbers = list(filter(is_prime, numbers)) #жаңа массивке салдық
-
-print("Жай сандар:", prime_numbers)
+a = Prime(numbers)
+print(a.isPrime())
